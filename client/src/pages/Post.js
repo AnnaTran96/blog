3603f11f8adc16c7923579c3d94a915d25594e05
Anna Tran <annatran@annas-air.mynet>
2020-09-03 18:06:01 +0100

    add edit and delete buttons

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -19,12 +19,42 @@ async componentDidMount(){
     this.setState({name: data.post[0].name})
     this.setState({story: data.post[0].story})
 }
+
+// deletePost = (div) => {
+//     const options = {
+//         method: 'DELETE'
+//     }
+//     fetch(`http://localhost:3000/posts/${this.state.postID}`, options)
+//         .then(div.remove())
+//         .catch(console.warn)
+// }
+
+// updatePost = (div) => {
+//     const options = {
+//         method: 'PATCH'
+//     }
+//     fetch(`http://localhost:3000/posts/${this.state.postID}`, options)
+//         .then
+// }
+
+// handleEdit = (e) => {
+//     e.preventDefault();
+//     this.setState({title: e.target.value.title, name: e.target.value.name, story: e.target.value.story})
+// }
+
+// handleDelete = (e) => {
+//     e.preventDefault();
+//     this.deletePost()
+// }
+
 render(){
     return (
         <div>
             <h1>{this.state.title}</h1>
             <h3>{this.state.name}</h3>
             <p>{this.state.story}</p>
+            {/* <button onClick={this.handleEdit}>Edit</button> */}
+            {/* <button onClick={this.handleDelete}>Delete</button> */}
         </div>
     )
 }
