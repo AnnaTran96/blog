23683124eae5847a8f9a079e5acef2818f7f263c
Anna Tran <annatran@annas-air.mynet>
2020-09-03 18:05:24 +0100

    create update and destroy queries

--- server-postgres/databases/queries.js
+++ server-postgres/databases/queries.js
@@ -1,5 +1,8 @@
 const index = `SELECT * FROM post`;
 const show = `SELECT * FROM post WHERE id = $1`;
 const create = `INSERT INTO post (title, name, story) VALUES ($1, $2, $3) RETURNING *`;
+const update = `UPDATE post SET title = $1 , name = $2, story= $3  WHERE id = $4 RETURNING *`; 
+const destroy = `DELETE FROM posts WHERE id = $1`;
 
-module.exports = { index, show, create };
+
+module.exports = { index, show, create, update, destroy };
