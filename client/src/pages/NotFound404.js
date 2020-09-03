926cf365448b11c81ce2b81478191db207dbbf16
Anna Tran <annatran@annas-air.mynet>
2020-09-03 17:09:02 +0100

    add not found page

---
+++ client/src/pages/NotFound404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Link } from 'react-router-dom'
+
+const NotFound404 = () => {
+    return (
+        <div>
+            <p>Sorry this page does not exist</p>
+            {/* <Link to='/'>Go Back to Home</Link> */}
+        </div>
+    )
+}
+
+export default NotFound404;
